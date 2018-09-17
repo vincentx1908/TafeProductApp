fcbd0474da80dc27f19106b03c4344a3d6814323
Zhoukang Xiong <001066207@tafesa.edu.au>
2018-09-17 15:18:35 +0930

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 chargeWithWrapBlock.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                chargeWithGST.Text = Convert.ToString((cProduct.TotalPayment + 25 + 5) * 110 / 100);
             }
             catch (FormatException)
             {
@@ -51,6 +52,7 @@ namespace ProductApps
             totalPaymentTextBlock.Text = "";
             totalChargeTextBlock.Text = "";
             chargeWithWrapBlock.Text = "";
+            chargeWithGST.Text = "";
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
